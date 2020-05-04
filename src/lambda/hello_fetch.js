24eb9f181ff138b5ed946cda4ceae748b3ea8f47
Shadab <shadab14meb346@gmail.com>
2020-05-04 20:15:17 +0530

    test fetch

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -1,13 +1,15 @@
 import fetch from "node-fetch";
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
-
+// const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const perPage = 20;
+const currentPage = 1;
+const API_ENDPOINT = `https://api.github.com/repos/open-source-ideas/open-source-ideas/issues?per_page=${perPage}&page=${currentPage}`;
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { "Accept": "application/json" } })
-    .then(response => response.json())
-    .then(data => ({
-      statusCode: 200,
-      body: data.joke
-    }))
-    .catch(error => ({ statusCode: 422, body: String(error) }));
+	return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+		.then((response) => response.json())
+		.then((data) => ({
+			statusCode: 200,
+			body: data,
+		}))
+		.catch((error) => ({ statusCode: 422, body: String(error) }));
 };
