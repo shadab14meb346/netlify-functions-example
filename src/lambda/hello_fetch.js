64468d53f21c122d1a56d0bc6823541b0a090627
Shadab <shadab14meb346@gmail.com>
2020-05-04 20:23:54 +0530

    test fetch

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -3,7 +3,8 @@ import fetch from "node-fetch";
 // const API_ENDPOINT = "https://icanhazdadjoke.com/";
 const perPage = 20;
 const currentPage = 1;
-const API_ENDPOINT = `https://api.github.com/repos/open-source-ideas/open-source-ideas/issues?per_page=20&page=1`;
+const API_ENDPOINT =
+	"https://api.github.com/repos/open-source-ideas/open-source-ideas/issues?per_page=20&page=1";
 exports.handler = async (event, context) => {
 	return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
 		.then((response) => response.json())
