c6c0cb7957ac4bb56088e4992d634f25ea200d33
Shadab <shadab14meb346@gmail.com>
2020-05-04 20:36:10 +0530

    test github get api

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -1,16 +1,16 @@
 import fetch from "node-fetch";
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+// const API_ENDPOINT = "https://icanhazdadjoke.com/";
 const perPage = 20;
 const currentPage = 1;
-// const API_ENDPOINT =
-// 	"https://api.github.com/repos/open-source-ideas/open-source-ideas/issues?per_page=20&page=1";
+const API_ENDPOINT =
+	"https://api.github.com/repos/open-source-ideas/open-source-ideas/issues?per_page=20&page=1";
 exports.handler = async (event, context) => {
 	return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
 		.then((response) => response.json())
 		.then((data) => ({
 			statusCode: 200,
-			body: data.joke,
+			body: data,
 		}))
 		.catch((error) => ({ statusCode: 422, body: String(error) }));
 };
