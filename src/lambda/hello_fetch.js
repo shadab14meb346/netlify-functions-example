38e4eb7e442c9314985488de0ea5b25370c8f92a
Shadab <shadab14meb346@gmail.com>
2020-05-05 02:31:23 +0530

    test starwars get api

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -5,7 +5,7 @@ const currentPage = 1;
 const API_ENDPOINT = "https://swapi.dev/api/people/1/";
 
 exports.handler = async (event, context) => {
-	return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+	return fetch(API_ENDPOINT)
 		.then((response) => response.json())
 		.then((data) => ({
 			statusCode: 200,
