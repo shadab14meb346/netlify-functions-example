3f240dc941662b043fc120c64e73169df5259d58
Shadab <shadab14meb346@gmail.com>
2020-05-04 20:31:21 +0530

    test fetch

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -10,7 +10,7 @@ exports.handler = async (event, context) => {
 		.then((response) => response.json())
 		.then((data) => ({
 			statusCode: 200,
-			body: data,
+			body: data.joke,
 		}))
 		.catch((error) => ({ statusCode: 422, body: String(error) }));
 };
