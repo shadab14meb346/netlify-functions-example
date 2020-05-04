1578b1c7e51232cf868f1b65af8864da27a234ff
Shadab <shadab14meb346@gmail.com>
2020-05-05 02:51:25 +0530

    test starwars get api using axios

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -7,9 +7,9 @@ const API_ENDPOINT = "https://swapi.dev/api/people/1/";
 exports.handler = async (event, context) => {
 	return axios
 		.get(API_ENDPOINT)
-		.then((data) => ({
+		.then((response) => ({
 			statusCode: 200,
-			body: JSON.stringify(data),
+			body: JSON.stringify(response.data),
 		}))
 		.catch((error) => ({ statusCode: 422, body: String(error) }));
 };
