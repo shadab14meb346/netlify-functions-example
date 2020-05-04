327ea3351571c618e73b41408c88d0a21652408e
Shadab <shadab14meb346@gmail.com>
2020-05-04 20:28:21 +0530

    test fetch

--- src/lambda/hello_fetch.js
+++ src/lambda/hello_fetch.js
@@ -6,7 +6,7 @@ const currentPage = 1;
 // const API_ENDPOINT =
 // 	"https://api.github.com/repos/open-source-ideas/open-source-ideas/issues?per_page=20&page=1";
 exports.handler = async (event, context) => {
-	return fetch(API_ENDPOINT)
+	return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
 		.then((response) => response.json())
 		.then((data) => ({
 			statusCode: 200,
